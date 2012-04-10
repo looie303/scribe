37f9dfe90eeabecef6a52f71d0cac2027e655b39
Greg Lewis <greg.lewis@ppfa.org>
2012-04-10 10:47:54 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scribe::Application.config.secret_token = 'c8c6b0fdbd349e7e219542e664de07eab20e1f1ddd0eafd245de627469c687b8e4772692dc84993b707e75da7c59390bbc47282f06248a45969f9afe90303b29'
